f3b8817f1d15025605386b00cd056fc2a9bdac80
ebertm777 <johndoe@example.comeber.tmacedo@gmail.com>
2025-02-21 10:07:12 -0300

    removed contact link

--- app/Constants/SiteData.js
+++ app/Constants/SiteData.js
@@ -1,5 +1,5 @@
 export const menuData = [
-  { href: "#", name: "Contact" },
+  // { href: "#", name: "Contact" },
   { href: "#", name: "Projects" },
   { href: "https://www.linkedin.com/in/ebermacedo/", name: "Linkedin" },
   { href: "https://github.com/ebertm777", name: "Github" },
