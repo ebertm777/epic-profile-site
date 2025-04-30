739e242f10c1b1eef28faf7732963aa980671bf5
ebertm777 <johndoe@example.comeber.tmacedo@gmail.com>
2025-04-30 14:47:40 -0300

    whatever

---
+++ app/Components/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+"use client";
+import * as S from "./styles";
+
+export default function Projects() {
+  return (
+    <S.Container>
+      <>
+        <></>
+      </>
+    </S.Container>
+  );
+}
