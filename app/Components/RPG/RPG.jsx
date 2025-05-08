2ef107c6e008b3c52574aede1fdf9ee0f160c1fc
ebertm777 <johndoe@example.comeber.tmacedo@gmail.com>
2025-05-08 13:35:42 -0300

    added dynamic text to iframe button

--- app/Components/RPG/RPG.jsx
+++ app/Components/RPG/RPG.jsx
@@ -3,24 +3,28 @@ import { useState } from "react";
 import * as S from "./styles";
 
 export default function Rpg() {
-    const [open, setOpen] = useState(false);
-    const handleToggle = () => {
-      setOpen((prev) => !prev);
-    };
-  
-    return (
-      <>
-        <S.ShowRpgModel onClick={handleToggle}>Click me to play</S.ShowRpgModel>
-        <S.Container $isActive={open}>
-          <iframe
-            src="https://dungeon-crawler-cards.vercel.app/"
-            title="Flourish Story"
-            width="100%"
-            height="100%"
-            allowFullScreen
-          ></iframe>
-        </S.Container>
-      </>
-    );
-  }
-  
+  const [open, setOpen] = useState(false);
+  const handleToggle = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <S.ShowRpgModel onClick={handleToggle}>
+        {open ? "Fechar RPG" : "Abrir RPG"}
+        <span role="img" aria-label="RPG">
+          &nbsp;🎲
+        </span>
+      </S.ShowRpgModel>
+      <S.Container $isActive={open}>
+        <iframe
+          src="https://dungeon-crawler-cards.vercel.app/"
+          title="Flourish Story"
+          width="100%"
+          height="100%"
+          allowFullScreen
+        ></iframe>
+      </S.Container>
+    </>
+  );
+}
