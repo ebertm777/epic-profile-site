0086e8774d56c5f0ae94740b4a3a5d3d8c7be57f
ebertm777 <eber.tmacedo@gmail.com>
2026-04-29 10:55:19 -0300

    added new console msg

--- app/page.js
+++ app/page.js
@@ -2,18 +2,12 @@
 import Main from "./Main/Main.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
 import NavBar from "./Components/Navbar/Navbar.jsx";
-
-const Azem =
-  "Herein I commit the chronicle of the traveler. Shepherd to the stars in the dark.";
-
-console.log(
-  "%c " + Azem,
-  "background: #662d91; color: #ffffff; padding: 2px 4px; border-radius: 2px;"
-);
+import Chronicle from "./Main/Chronicle.jsx";
 
 export default function Home() {
   return (
     <>
+    <Chronicle/>
       <NavBar />
       <Main />
       <Footer />
